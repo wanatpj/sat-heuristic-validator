476fc78488aa41134b695a9949481bed4f9b9165
Paweł Wanat <wanatpj@gmail.com>
2023-08-18 17:54:04 +0100

    make conf aggregate return accuracy, precission and recall metrics

--- src/three_sat/aggregate.py
+++ src/three_sat/aggregate.py
@@ -1,8 +1,27 @@
+import math
 from abc import abstractmethod
+from dataclasses import dataclass
 
+import numpy as np
 import sklearn.metrics
 
 from three_sat.struct import And
+
+
+@dataclass(eq=False)
+class PerformanceMetrics:
+    accuracy: float
+    precision: np.ndarray
+    recall: np.ndarray
+    confusion_matrix: np.ndarray
+
+    def __eq__(self, other: "PerformanceMetrics") -> bool:
+        return (
+            math.isclose(self.accuracy, other.accuracy)
+            and np.isclose(self.precision, other.precision).all()
+            and np.isclose(self.recall, other.recall).all()
+            and np.isclose(self.confusion_matrix, other.confusion_matrix).all()
+        )
 
 
 class AggregationError(Exception):
@@ -31,14 +50,31 @@ class ConfusionAggregator(Aggregator):
         self.y_pred.append(y_actual)
 
     def done(self):
-        print(sklearn.metrics.confusion_matrix(y_true=self.y_true, y_pred=self.y_pred))
+        conf_mat = sklearn.metrics.confusion_matrix(y_true=self.y_true, y_pred=self.y_pred)
+        return PerformanceMetrics(
+            accuracy = conf_mat.diagonal().sum() / conf_mat.sum(),
+            precision = conf_mat.diagonal() / conf_mat.sum(axis=0),
+            recall = conf_mat.diagonal() / conf_mat.sum(axis=1),
+            confusion_matrix = conf_mat,
+        )
 
 
 class SampleAggregator(Aggregator):
 
     def aggregate(self, y_actual: bool, x: And, y_expected: bool):
         if y_actual != y_expected:
-            raise AggregationError(f"{y_actual=} {x=} {y_expected=}")
+            raise AggregationError(f"{y_actual=} x={x} {y_expected=}")
+
+    def done(self):
+        pass
+
+
+
+class AllSamplesAggregator(Aggregator):
+
+    def aggregate(self, y_actual: bool, x: And, y_expected: bool):
+        if y_actual != y_expected:
+            print(f"{y_actual=} x={x} {y_expected=}")
 
     def done(self):
         pass
--- tests/three_sat/test_aggregate.py
+++ tests/three_sat/test_aggregate.py
@@ -1,19 +1,16 @@
 from unittest.mock import ANY
 
+import numpy as np
 import pytest
 
-from three_sat.aggregate import AggregationError, ConfusionAggregator, SampleAggregator
+from three_sat.aggregate import AggregationError, ConfusionAggregator, PerformanceMetrics, SampleAggregator
 
 
-def test_sample_aggregator(capsys):
+def test_sample_aggregator():
     aggregator = SampleAggregator()
 
     aggregator.aggregate(y_actual=True, x=ANY, y_expected=True)
     aggregator.aggregate(y_actual=False, x=ANY, y_expected=False)
-
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == ""
 
     with pytest.raises(AggregationError):
         aggregator.aggregate(y_actual=True, x=ANY, y_expected=False)
@@ -21,34 +18,26 @@ def test_sample_aggregator(capsys):
     with pytest.raises(AggregationError):
         aggregator.aggregate(y_actual=False, x=ANY, y_expected=True)
 
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == ""
 
-
-def test_confusion_aggregator(capsys):
+def test_confusion_aggregator():
     aggregator = ConfusionAggregator()
 
     aggregator.aggregate(y_actual=True, x=ANY, y_expected=True)
-    aggregator.done()
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == "[[1]]\n"
 
     aggregator.aggregate(y_actual=True, x=ANY, y_expected=False)
-    aggregator.done()
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == "[[0 1]\n [0 1]]\n"
+    aggregator.aggregate(y_actual=True, x=ANY, y_expected=False)
 
     aggregator.aggregate(y_actual=False, x=ANY, y_expected=True)
-    aggregator.done()
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == "[[0 1]\n [1 1]]\n"
+    aggregator.aggregate(y_actual=False, x=ANY, y_expected=True)
+    aggregator.aggregate(y_actual=False, x=ANY, y_expected=True)
 
     aggregator.aggregate(y_actual=False, x=ANY, y_expected=False)
-    aggregator.done()
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert captured.out == "[[1 1]\n [1 1]]\n"
+    aggregator.aggregate(y_actual=False, x=ANY, y_expected=False)
+    aggregator.aggregate(y_actual=False, x=ANY, y_expected=False)
+    aggregator.aggregate(y_actual=False, x=ANY, y_expected=False)
+    assert aggregator.done() == PerformanceMetrics(
+        accuracy=5 / 10,
+        precision=np.array([4 / 7, 1 / 3]),
+        recall=np.array([4 / 6, 1 / 4]),
+        confusion_matrix=[[4, 2], [3, 1]],
+    )
